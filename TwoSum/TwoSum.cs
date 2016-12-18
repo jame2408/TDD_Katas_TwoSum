dd62c3d9d6d39eaf85125a1ff246d572e83698bc
jame2408 <jame2408@gmail.com>
2016-12-18 16:31:46 +0800

    Given an array of integers, return indices of the two numbers such that they add up to a specific target.

---
+++ TwoSum/TwoSum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace TwoSum
+{
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> valueIndex = new Dictionary<int, int>();
+
+            if (nums.Length < 2)
+            {
+                return new int[] { 0, 0};
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (valueIndex.ContainsKey(complement))
+                {
+                    return new[] { valueIndex[complement], i };
+                }
+
+                if (!valueIndex.ContainsKey(nums[i]))
+                {
+                    valueIndex.Add(nums[i], i);
+                }
+            }
+
+            return new int[] { 0, 0 };
+        }
+    }
+}
---
+++ TwoSumTests/TwoSumTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwoSum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum.Tests
+{
+    [TestClass()]
+    public class TwoSumTests
+    {
+        [TestMethod()]
+        public void NullTest()
+        {
+            //Arrange
+            int[] nums = { };
+            var TwoSum = 0;
+
+            //Act
+            var target = new TwoSumSolution();
+            var expected = target.TwoSum(nums, TwoSum);
+
+            //Assert
+            int[] actual = new int[2];
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void NoTwoSumTest()
+        {
+            //Arrange
+            int[] nums = { 1, 2, 3, 4, 5 };
+            var TwoSum = 12;
+
+            //Act
+            var target = new TwoSumSolution();
+            var expected = target.TwoSum(nums, TwoSum);
+
+            //Assert
+            int[] actual = new int[2];
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+            //Arrange
+            int[] nums = { 2, 7, 11, 15 };
+            var TwoSum = 26;
+
+            //Act
+            var target = new TwoSumSolution();
+            var expected = target.TwoSum(nums, TwoSum);
+
+            //Assert
+            int[] actual = new int[2]{ 2, 3};
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
